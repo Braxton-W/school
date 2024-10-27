b1ae6b54b2e492c0c8654ca82895ce051e1a0a1e
Braxton-W <149643698+Braxton-W@users.noreply.github.com>
2024-10-27 17:21:56 -0400

    Create Shape.java

---
+++ java/OOP_Part2/shape/Shape.java
@@ -0,0 +1,10 @@
+package shape;
+
+/**
+ * Shape interface to initialize methods for shapes
+ * @author Braxton Worsley
+ */
+public interface Shape {
+	public double getArea();
+	public double getPerimeter();
+}
