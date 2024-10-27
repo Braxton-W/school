d25b0db6b7813bc7caf5eb078c6952677dd52196
Braxton-W <149643698+Braxton-W@users.noreply.github.com>
2024-10-27 17:21:05 -0400

    Create Numbers.java

---
+++ java/OOP_Part2/numbers/Numbers.java
@@ -0,0 +1,10 @@
+package numbers;
+
+/**
+ * Numbers interface to initialize methods
+ * @author Braxton Worsley
+ */
+public interface Numbers {
+	public Numbers add(Numbers other);
+	public Numbers subtract(Numbers other);
+}
